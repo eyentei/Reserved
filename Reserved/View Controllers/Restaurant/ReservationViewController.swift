800abbd48653abdc97cc21d2213b1c636e7e8f81
Maria Korovina <eyentei@gmail.com>
2019-12-09 22:51:05 +0300

    table selection

--- Reserved/View Controllers/Restaurant/ReservationViewController.swift
+++ Reserved/View Controllers/Restaurant/ReservationViewController.swift
@@ -10,13 +10,38 @@ import UIKit
 
 class ReservationViewController: UIViewController {
 
+    
+    @IBOutlet var buttons: [UIButton]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        buttons[4].isEnabled = false
+        buttons[8].isEnabled = false
+        
+        for button in buttons {
+            button.setImage(UIImage.init(named: String(button.tag)+"w"), for: UIControl.State.normal)
+            button.setImage(UIImage.init(named: String(button.tag)+"g"), for: UIControl.State.disabled)
+            button.setImage(UIImage.init(named: String(button.tag)+"s"), for: UIControl.State.selected)
+            button.adjustsImageWhenHighlighted = false
+        }
+
 
         // Do any additional setup after loading the view.
     }
     
 
+    @IBAction func tableClicked(_ sender: UIButton) {
+    
+        if sender.isSelected {
+            sender.isSelected = false
+        } else {
+            sender.isSelected = true
+        }
+        
+    }
+    
     /*
     // MARK: - Navigation
 
