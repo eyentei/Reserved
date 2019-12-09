99748ded0e3619443b8dc6bb98b079fdb09365e3
Maria Korovina <eyentei@gmail.com>
2019-12-10 02:30:02 +0300

    time picking

---
+++ Reserved/View Controllers/Restaurant/DatePickerViewController.swift
@@ -0,0 +1,80 @@
+//
+//  DatePickerViewController.swift
+//  Reserved
+//
+//  Created by Мария Коровина on 09/12/2019.
+//  Copyright © 2019 Мария Коровина. All rights reserved.
+//
+
+import UIKit
+
+class DatePickerViewController: UIViewController {
+    
+    var delegate: ModalDelegate?
+    @IBOutlet weak var datepickerView: UIView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    @IBOutlet weak var error: UILabel!
+    
+    @objc func datePickerChanged() {
+        var components = Calendar.current.dateComponents([.hour, .minute, .month, .year, .day], from: datePicker.date)
+
+        if components.hour! < 7 {
+            components.hour = 7
+            components.minute = 0
+            error.text = "Please pick hours from 7:00 to 21:00"
+            datePicker.setDate(Calendar.current.date(from: components)!, animated: true)
+        }
+        else if components.hour! > 21 {
+            components.hour = 21
+            components.minute = 0
+            error.text = "Please pick hours from 7:00 to 21:00"
+            datePicker.setDate(Calendar.current.date(from: components)!, animated: true)
+            
+        }
+        else {
+            error.text = ""
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        error.text = ""
+        datePicker.minimumDate = Date()
+        datePickerChanged()
+        datePicker.minuteInterval = 15
+        datepickerView.layer.cornerRadius = 24
+        datePicker.addTarget(self, action: #selector(datePickerChanged), for: .valueChanged)
+        
+        self.definesPresentationContext = true
+        
+        let blurEffect = UIBlurEffect(style: .dark)
+        let blurEffectView = UIVisualEffectView(effect: blurEffect)
+        blurEffectView.alpha = 0.4
+        blurEffectView.frame = self.view.bounds
+        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+
+        view.insertSubview(blurEffectView, at: 0)
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func ok(_ sender: Any) {
+        
+        
+        if let delegate = self.delegate {
+            delegate.changeValue(value: datePicker!.date)
+        }
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Reserved/View Controllers/Restaurant/ReservationViewController.swift
+++ Reserved/View Controllers/Restaurant/ReservationViewController.swift
@@ -8,15 +8,62 @@
 
 import UIKit
 
-class ReservationViewController: UIViewController {
-
+protocol ModalDelegate {
+    func changeValue(value: Date)
+}
+class ReservationViewController: UIViewController, ModalDelegate {
+    
+    
     
     @IBOutlet var buttons: [UIButton]!
-    
+    @IBOutlet weak var date: UILabel!
+    @IBAction func showDatePicker(_ sender: UIButton) {
+        let vc = self.storyboard!.instantiateViewController(withIdentifier: "datepickerVC") as! DatePickerViewController
+       vc.modalPresentationStyle = .overCurrentContext
+       vc.modalTransitionStyle = .crossDissolve
+       vc.delegate=self
+       present(vc, animated: true, completion: nil)
+        
+       }
+
+    func formatDate(value: Date)-> String{
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.y HH:mm"
+        return dateFormatter.string(from: value)
+    }
+    func changeValue(value: Date) {
+        date.text = formatDate(value: value)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        var components = Calendar.current.dateComponents([.hour, .minute, .month, .year, .day], from: Date())
+
+        if components.hour! < 7 {
+            components.hour = 7
+            components.minute = 0
+            let d = Calendar.current.date(from: components)
+            date.text = formatDate(value: d!)
+            
+        } else if (components.hour! == 21 && components.minute! > 29) || components.hour! > 21 {
+
+            components.day = components.day!+1
+            components.hour = 7
+            components.minute = 0
+            let d = Calendar.current.date(from: components)
+            date.text = formatDate(value: d!)
+    
+        } else {
+            let calendar = Calendar.current
+            let rightNow = Date()
+            let interval = 15
+            let nextDiff = interval - calendar.component(.minute, from: rightNow) % interval
+            let nextDate = calendar.date(byAdding: .minute, value: nextDiff, to: rightNow) ?? Date()
+            date.text = formatDate(value: nextDate)
+        }
+       
         buttons[4].isEnabled = false
         buttons[8].isEnabled = false
         
