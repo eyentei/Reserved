c49b715382768d9e751d6bab14581148a16a36c0
Elena <orshanksayaelena@gmail.com>
2019-12-10 23:05:28 +0300

    Very simple changing of user's settings

--- Reserved/View Controllers/Profile/ProfileSettingsViewController.swift
+++ Reserved/View Controllers/Profile/ProfileSettingsViewController.swift
@@ -14,6 +14,10 @@ class ProfileSettingsViewController: UIViewController {
     @IBOutlet weak var Phone: UITextField!
     @IBOutlet weak var Mail: UITextField!
     @IBOutlet weak var Name: UITextField!
+    
+    @IBOutlet weak var RepeatPassword: UITextField!
+    @IBOutlet weak var Password: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let realm = try! Realm()
@@ -24,7 +28,32 @@ class ProfileSettingsViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-  
+    
+    
+    @IBAction func SaveChanges(_ sender: Any) {
+        let realm = try! Realm()
+               var users = realm.objects(User.self)
+        try! realm.write {
+            users.first?.setValue(Name.text, forKey: "name")
+            users.first?.setValue(Mail.text, forKey: "email")
+            users.first?.setValue(Phone.text, forKey: "number")
+        }
+            if Password.text==RepeatPassword.text{
+                try! realm.write{  users.first?.setValue(Password.text, forKey: "password")}
+ }
+            else{
+                        let alert = UIAlertController(title: "Passwords do not match!", message: "Please, try again", preferredStyle: UIAlertController.Style.alert)
+
+                        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+
+                        self.present(alert, animated: true, completion: nil)
+                    }
+                }
+                   
+            
+
+        
+    
     /*
     // MARK: - Navigation
 
@@ -34,5 +63,6 @@ class ProfileSettingsViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+
 
 }
