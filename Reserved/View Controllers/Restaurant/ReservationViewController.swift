e9a178482085cf6c1f3bdd0f6ae77477912c52e0
Maria Korovina <eyentei@gmail.com>
2019-12-12 17:02:31 +0300

    reservations are now saved

--- Reserved/View Controllers/Restaurant/ReservationViewController.swift
+++ Reserved/View Controllers/Restaurant/ReservationViewController.swift
@@ -7,20 +7,56 @@
 //
 
 import UIKit
+import RealmSwift
 
 protocol ModalDelegate {
     func changeValue(value: Date)
 }
 class ReservationViewController: UIViewController, ModalDelegate {
     
-    
-    
+    var currentUser:User!
+    var realm: Realm!
+    var reservationDate: Date!
+    var currentRestaurant: Restaurant!
     
     @IBOutlet var buttons: [UIButton]!
     @IBOutlet weak var date: UILabel!
     
     @IBAction func selectTable(_ sender: UIButton) {
-        navigationController?.popViewController(animated: true)
+        realm = try! Realm()
+        
+        let tables=List<Int>()
+        for button in buttons {
+            if button.isSelected {
+                tables.append(button.tag)
+            }
+        }
+        
+        try! realm.write() {
+            if let result = currentUser.reservations.filter("restaurant == %@ AND time == %@", currentRestaurant!, reservationDate!).first {
+                
+        
+              result.tables.removeAll()
+              result.tables.append(objectsIn: tables)
+                
+            } else {
+            
+            let reservation = Reservation()
+                
+                reservation.person = currentUser
+                reservation.restaurant  = currentRestaurant
+                
+                reservation.tables = tables
+                reservation.time = reservationDate
+                
+            currentUser.reservations.append(reservation)
+                realm.add(reservation)
+            
+            }
+            
+        }
+    
+    navigationController?.popViewController(animated: true)
     }
     
     @IBAction func showDatePicker(_ sender: UIButton) {
@@ -38,12 +74,31 @@ class ReservationViewController: UIViewController, ModalDelegate {
         return dateFormatter.string(from: value)
     }
     func changeValue(value: Date) {
+        reservationDate = value
         date.text = formatDate(value: value)
+        let tables = List<Int>()
+
+        if let reservation = currentUser.reservations.filter("restaurant == %@ AND time == %@", currentRestaurant!, reservationDate!).first {
+        tables.append(objectsIn: reservation.tables)
+        }
+        
+        for button in buttons {
+            
+            if tables.contains(button.tag)  {
+                button.isSelected = true
+            } else {
+                button.isSelected = false
+            }
+        }
+    
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        realm = try! Realm()
+
+        let pk = UserDefaults.standard.string(forKey: "UserId")!
+        currentUser = realm.object(ofType: User.self, forPrimaryKey: pk)
         
         var components = Calendar.current.dateComponents([.hour, .minute, .month, .year, .day], from: Date())
 
@@ -51,6 +106,7 @@ class ReservationViewController: UIViewController, ModalDelegate {
             components.hour = 7
             components.minute = 0
             let d = Calendar.current.date(from: components)
+            reservationDate = d
             date.text = formatDate(value: d!)
             
         } else if (components.hour! == 21 && components.minute! > 29) || components.hour! > 21 {
@@ -58,30 +114,42 @@ class ReservationViewController: UIViewController, ModalDelegate {
             components.day = components.day!+1
             components.hour = 7
             components.minute = 0
+
             let d = Calendar.current.date(from: components)
+            reservationDate = d
             date.text = formatDate(value: d!)
     
         } else {
             let calendar = Calendar.current
-            let rightNow = Date()
+            let rightNow = calendar.date(from: calendar.dateComponents([.year, .month, .day, .hour, .minute], from: Date()))
+            
             let interval = 15
-            let nextDiff = interval - calendar.component(.minute, from: rightNow) % interval
-            let nextDate = calendar.date(byAdding: .minute, value: nextDiff, to: rightNow) ?? Date()
-            date.text = formatDate(value: nextDate)
+            let nextDiff = interval - calendar.component(.minute, from: rightNow!) % interval
+            print(nextDiff)
+            let nextDate = calendar.date(byAdding: .minute, value: nextDiff, to: rightNow!)
+          
+            reservationDate = nextDate
+            date.text = formatDate(value: nextDate!)
         }
-       
-        buttons[4].isEnabled = false
-        buttons[8].isEnabled = false
+
+        let tables = List<Int>()
+
+        if let reservation = currentUser.reservations.filter("restaurant == %@ AND time == %@", currentRestaurant!, reservationDate!).first {
+            tables.append(objectsIn: reservation.tables)
+        }
+    
         
         for button in buttons {
             button.setImage(UIImage.init(named: String(button.tag)+"w"), for: UIControl.State.normal)
             button.setImage(UIImage.init(named: String(button.tag)+"g"), for: UIControl.State.disabled)
             button.setImage(UIImage.init(named: String(button.tag)+"s"), for: UIControl.State.selected)
             button.adjustsImageWhenHighlighted = false
+            
+            if tables.contains(button.tag)  {
+                button.isSelected = true
+            }
         }
 
-
-        // Do any additional setup after loading the view.
     }
     
 
